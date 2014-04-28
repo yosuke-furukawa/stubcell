b2050694ff9bbd5848531079b634178f32039ac2
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-04-28 13:26:21 +0900

    Add tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs = require("fs");
+var JSON5 = require("json5");
 var yaml = require('yamljs');
 var _ = require('lodash');
 var express = require('express');
@@ -13,21 +14,46 @@ StubCell.prototype.loadEntry = function(entryPath) {
   this.entries = yaml.load(entryPath);
 };
 
+StubCell.prototype._parseJSON = function(data, isJSON5) {
+  var result;
+  try {
+    if (isJSON5) {
+      result = JSON5.parse(""+data);
+    } else {
+      result = JSON.parse(data);
+    }
+  } catch (e) {
+    throw e;
+  }
+  return result;
+};
+
 StubCell.prototype.server = function() {
   this.entries.forEach(function(entry) {
     var method = entry.request.method.toLowerCase();
     var response = entry.response;
     var headers = response.headers;
     var file = response.file;
+    var ext = path.extname(file);
     app[method](entry.request.url, function(req, res) {
+      var expectJSON = false;
       Object.keys(headers).forEach(function(key) {
         res.setHeader(key, headers[key]);
+        if (key === "content-type" && headers[key] === "application/json") {
+          expectJSON = true;
+        }
       });
       res.statusCode = response.status;
-      var readstream = fs.createReadStream(file);
-      // TODO JSON5Stream or JSONStream
-      readstream.pipe(res);
-    });
-  });
+      fs.readFile(file, function(err, data) {
+        if (err) throw err;
+        var jsonData;
+        if (expectJSON) {
+          data = this._parseJSON(data, ext === ".json5");
+        }
+        res.send(data);
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
   return app;
-}
+};
+
---
+++ test/test.js
@@ -0,0 +1,56 @@
+var StubCell = require("../index");
+var stubcell = new StubCell();
+var http = require("http");
+var assert = require("power-assert");
+
+stubcell.loadEntry(__dirname + "/example.yaml");
+var app = stubcell.server();
+
+describe('Stubcell server', function(){
+  var server;
+  beforeEach(function(done) {
+    server = app.listen(3000);
+    server.on("listening", done);
+  });
+  afterEach(function(done) {
+    server.on("close", done);
+    server.close();
+  });
+  describe("request", function(){
+    it("should return hello world for example.json5", function(done){
+      http.get("http://localhost:3000/test/abc", function(res){
+        var data = '';
+        res.on("data", function(chunk) {
+          data += chunk;
+        });
+        res.on("end", function() {
+          try {
+            assert.equal("Hello world", JSON.parse(data).message);
+            done();
+          } catch (e) {
+            console.error(e);
+          }
+        });
+      });
+
+    });
+    it("should return hello world for example.json", function(done){
+      http.get("http://localhost:3000/test/", function(res){
+        var data = '';
+        res.on("data", function(chunk) {
+          data += chunk;
+        });
+        res.on("end", function() {
+          try {
+            assert.equal("Hello world world", JSON.parse(data).message);
+            done();
+          } catch (e) {
+            console.error(e);
+          }
+        });
+      });
+
+    });
+  });
+});
+
