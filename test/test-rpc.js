415a2b146104249bfc7ae94a8697fa27873b8c16
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-05-21 19:52:36 +0900

    Add querystring and body check

--- test/test-rpc.js
+++ test/test-rpc.js
@@ -3,7 +3,7 @@ var stubcell = new StubCell();
 var http = require("http");
 var assert = require("power-assert");
 
-stubcell.loadEntry(__dirname + "/example.yaml");
+stubcell.loadEntry(__dirname + "/example.yaml", "", true);
 var app = stubcell.server();
 describe('Stubcell server', function(){
   var server;
@@ -53,51 +53,3 @@ describe('Stubcell server', function(){
   });
 });
 
-describe('Stubcell server', function(){
-  var server;
-  beforeEach(function(done) {
-    server = app.listen(3000);
-    server.on("listening", done);
-  });
-  afterEach(function(done) {
-    server.on("close", done);
-    server.close();
-  });
-  describe("post jsonrpc request2", function(){
-
-    it("should return 333 for jsonrpc", function(done){
-
-      var opt = {
-        hostname: 'localhost',
-        port : 3000,
-        path : '/jsonrpc',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json' 
-        }
-      };
-      var req = http.request(opt, function(res){
-        var data = '';
-        res.on("data", function(chunk) {
-          data += chunk;
-        });
-        res.on("end", function() {
-          try {
-            assert.equal(JSON.parse(data).result, 333);
-            done();
-          } catch (e) {
-            done(e);
-          }
-        });
-      });
-      var reqBody = {
-        jsonrpc: 2.0,
-        method: 'subtract',
-        id: 1,
-        params: [456, 123]
-      };
-      req.write(JSON.stringify(reqBody));
-      req.end();
-    });
-  });
-});
