c64a6a77e984d9f3bad24ff4e6b2969f6cffbb8f
muddydixon <muddydixon@gmail.com>
2014-05-27 20:16:19 +0900

    add finally test

---
+++ test/test-finally.js
@@ -0,0 +1,40 @@
+var StubCell = require("../index");
+var http = require("http");
+var assert = require("power-assert");
+
+var stubcell = new StubCell();
+stubcell.loadEntry(__dirname + "/finally.yaml", {basepath: "", debug: true});
+var app = stubcell.server();
+describe('Stubcell server', function(){
+  var server;
+  beforeEach(function(done) {
+    server = app.listen(3000);
+    server.on("listening", done);
+  });
+  afterEach(function(done) {
+    server.on("close", done);
+    server.close();
+  });
+  describe("request", function(){
+    it("should return 'you look me!' when no match entries", function(done){
+      http.get("http://localhost:3000/dio", function(res){
+        var data = '';
+        console.log(res.statusCode);
+        assert.equal(res.statusCode, 200);
+        res.on("data", function(chunk) {
+          data += chunk;
+        });
+        res.on("end", function() {
+          try {
+            data = JSON.parse(data);
+            assert.equal(data.message, "you look me!");
+            done();
+          } catch (e) {
+            done(e);
+          }
+        });
+      });
+
+    });
+  });
+});
