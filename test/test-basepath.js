b23f3707129a4295ffdfe6d7fc541ad453e041bf
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-07-10 14:09:32 +0900

    Fix duplicate done

--- test/test-basepath.js
+++ test/test-basepath.js
@@ -62,13 +62,6 @@ describe('Stubcell server should set json basepath', function(){
             done(e);
           }
         });
-        fs.watch("./test/base/test/", {persistent: true },function(event, filename) {
-          console.log("changed file is : ", filename);
-          fs.readFile("./test/base/test/record_get.json", function(err, d) {
-            assert.deepEqual(JSON.parse(d), JSON.parse('{"test":"record"}'));
-            done();
-          });
-        });
       });
     });
   });
