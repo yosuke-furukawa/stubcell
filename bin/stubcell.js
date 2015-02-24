fc857b38e40de3bbc71f2aa4a7ba63f35a37898d
muddydixon <muddydixon@gmail.com>
2015-02-24 13:53:49 +0900

    added loose compare option

--- bin/stubcell.js
+++ bin/stubcell.js
@@ -10,6 +10,7 @@ program.version(package.version)
        .option("-b,--basepath [stub json basepath]", "json basepath, default is entry.yaml parent path ")
        .option("--record_target [record target server]", "record target server, default is null (no record file)")
        .option("-s,--silent", "hide detail info, default is false")
+       .option("-l,--loose", "compare loose")
        .parse(process.argv);
 
 var stubcell = new Stubcell();
@@ -21,7 +22,9 @@ var debug = !program.silent;
 var record = {};
 if(program.record_target) record.target = program.record_target;
 if(record.proxy) record.debug = debug;
-stubcell.loadEntry(entry, {debug: debug, basepath: basepath, record: record});
+stubcell.loadEntry(entry, {
+  debug: debug, basepath: basepath, record: record, looseCompare: program.loose
+});
 var app = stubcell.server();
 
 var app = app.listen(port);
@@ -30,4 +33,5 @@ app.on("listening", function(){
   console.log("\033[32m" + "entry yaml is " + entry+"\033[39m");
   console.log("\033[32m" + "silent is " + !debug+"\033[39m");
   console.log("\033[32m" + "record proxy is " + record.proxy+"\033[39m");
+  console.log("\033[32m" + "loose compare " + program.loose+"\033[39m");
 });
