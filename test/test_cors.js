e24fa160c6dcf22701aa3c3c6e9b35ce63aad094
muddydixon <muddydixon@gmail.com>
2015-03-04 17:20:47 +0900

    fixed test cors request headers

--- test/test_cors.js
+++ test/test_cors.js
@@ -15,6 +15,13 @@ describe('Stubcell server with cors', function(){
     var stubcell = new StubCell();
     stubcell.loadEntry(__dirname + "/cors.yaml", {basepath: "", debug: true, cors: true, port: 4000});
     var app = stubcell.server();
+    var createCorsHeader = function(origin, method, headers){
+      return {
+        "origin": origin,
+        "Access-Control-Request-Method": method,
+        "Access-Control-Request-Headers": typeof headers === "string" ? headers :  headers.join(", ")
+      };
+    };
 
     before(function(done) {
       server = app.listen(4000);
@@ -25,28 +32,47 @@ describe('Stubcell server with cors', function(){
       server.close();
     });
     it("GET request", function(done){
-      request({url: "http://localhost:4000/cors/1", method: "GET", proxy: ""}, function(err, res){
+      request({
+        url: "http://localhost:4000/cors/1",
+        method: "GET",
+        headers: {origin: "http://example.com"},
+        proxy: ""
+      }, function(err, res){
         assert(res.headers["access-control-allow-origin"], "*");
-        assert(res.headers["access-control-allow-methods"], "GET, POST, PUT, DELETE");
         done();
       });
     });
     it("POST request", function(done){
-      request({url: "http://localhost:4000/cors/1", method: "POST", proxy: ""}, function(err, res){
+      request({
+        url: "http://localhost:4000/cors/1",
+        method: "POST",
+        headers: createCorsHeader("http://example.com", "POST", "X-SAMPLE-Timestamp"),
+        proxy: ""
+      }, function(err, res){
         assert(res.headers["access-control-allow-origin"], "*");
         assert(res.headers["access-control-allow-methods"], "GET, POST, PUT, DELETE");
         done();
       });
     });
     it("PUT request", function(done){
-      request({url: "http://localhost:4000/cors/1", method: "PUT", proxy: ""}, function(err, res){
+      request({
+        url: "http://localhost:4000/cors/1",
+        method: "PUT",
+        headers: createCorsHeader("http://example.com", "PUT", "X-SAMPLE-Timestamp"),
+        proxy: ""
+      }, function(err, res){
         assert(res.headers["access-control-allow-origin"], "*");
         assert(res.headers["access-control-allow-methods"], "GET, POST, PUT, DELETE");
         done();
       });
     });
     it("DELETE request", function(done){
-      request({url: "http://localhost:4000/cors/1", method: "DELETE", proxy: ""}, function(err, res){
+      request({
+        url: "http://localhost:4000/cors/1",
+        method: "DELETE",
+        headers: createCorsHeader("http://example.com", "DELETE", "X-SAMPLE-Timestamp"),
+        proxy: ""
+      }, function(err, res){
         assert(res.headers["access-control-allow-origin"], "*");
         assert(res.headers["access-control-allow-methods"], "GET, POST, PUT, DELETE");
         done();
